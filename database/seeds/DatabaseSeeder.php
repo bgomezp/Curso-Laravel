83a0f0ad34177375a886df866b37d4874dcf8cc8
“bgomezp” <belisariogomez2012@gmail.com>
2019-08-06 22:06:51 -0500

    Seeder y Model Factory

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,19 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->truncateTablas(['rol','permiso']); // funcion para borra las tablas antes de sembarlas
+        $this->call(TablaRolSeeder::class); //llaamr a cad auna de los seeder para que se ejecute
+        $this->call(TablaPermisoSeeder::class);
+       
+
+    }
+    protected function truncateTablas(array  $tablas)
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0;');//quiatr el chequeo d etabals foraneas para que no genere erro ral borrarlas
+        foreach ($tablas as $tabla)
+        {
+            DB::table($tabla)->truncate(); //truncar o borrar tabla, dejarla limpia, sin registros 
+        }
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
     }
 }
