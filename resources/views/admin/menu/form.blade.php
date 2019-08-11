c543a124c389f89327020cee918d3945c6a2434e
“bgomezp” <belisariogomez2012@gmail.com>
2019-08-11 16:23:25 -0500

    agregado custom.css para cmapo requerido

--- resources/views/admin/menu/form.blade.php
+++ resources/views/admin/menu/form.blade.php
@@ -1,12 +1,12 @@
 <div class="form-group">
-    <label for="nombre" class="col-sm-3 control-label">Nombre</label>
+    <label for="nombre" class="col-sm-3 control-label requerido">Nombre</label>
 
     <div class="col-sm-9">
         <input type="txt" class="form-control" id="nombre" placeholder="Nombre">
     </div>
 </div>
 <div class="form-group">
-    <label for="url" class="col-sm-3 control-label">Url</label>
+    <label for="url" class="col-sm-3 control-label requerido">Url</label>
 
     <div class="col-sm-9">
         <input type="txt" class="form-control" id="url" placeholder="Url">
@@ -21,7 +21,7 @@
 </div>
 <!-- /.box-body -->
 <div class="box-footer">
-    <button type="submit" class="btn btn-default">Cancel</button>
+    <button type="submit" class="btn btn-default">Cancelar</button>
     <button type="submit" class="btn btn-info pull-right">Guardar</button>
 </div>
 <!-- /.box-footer -->
